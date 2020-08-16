fb4fdee26268f1fddab52a8dc85380a41746f308
Jenson <jensonjones2002@gmail.com>
2020-08-16 14:56:31 -0600

    Add files via upload

---
+++ tweet_cmd_line.py
@@ -0,0 +1,34 @@
+import requests
+import tweepy
+
+'''
+Apply for access to Twitters API here: https://developer.twitter.com/en/apply-for-access
+'''
+
+user = #your twitter username 
+
+'''
+Access your keys and tokens by going to the developer portal, then clicking
+your dev profile name at the top > Apps > Details > Keys and Tokens
+
+'''
+key = #API key
+secretKey = #API secret key 
+accessToken = #Your access token 
+accessToken_secret = #Your acess token secret
+
+auth = tweepy.OAuthHandler(key, secretKey)
+auth.set_access_token(accessToken, accessToken_secret)
+
+api = tweepy.API(auth)
+
+tweet = input('Enter tweet: ')
+try:
+    api.update_status(status=tweet)
+    print('success')
+    #you will then 
+except:
+    print('error')
+
+
+
